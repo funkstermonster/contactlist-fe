b1caa9b58d9ab59b116a7750c0ef4fac114b96a0
funkstermonster <fanni.szasz96@gmail.com>
2024-09-24 21:48:05 +0200

    set colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,44 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      extend: {
+        colors: {
+          grey: {
+            100: '#141414',
+            90: '#191919',
+            80: '#1E1E1E',
+            70: '#232323',
+            60: '#282828',
+            50: '#2D2D2D',
+            40: '#323232',
+            30: '#373737',
+            20: '#3C3C3C',
+            10: '#414141',
+          },
+          foreground: {
+            100: '#141414',
+            80: '#1E1E1E',
+          },
+          button: {
+            100: '#141414',
+            90: '#191919',
+            80: '#1E1E1E',
+            70: '#232323',
+            60: '#282828',
+          },
+          'button-state': {
+            default: '#282828',
+            hover: '#2D2D2D',
+            active: '#323232',
+          },
+        },
+        opacity: {
+          100: '1',
+          56: '0.56',
+          32: '0.32',
+        },
       },
     },
-  },
   plugins: [],
 };
 export default config;
