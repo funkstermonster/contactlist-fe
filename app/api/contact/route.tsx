e24307989e3f6f4d0b0df93a4c3aa61e16658f6e
funkstermonster <fanni.szasz96@gmail.com>
2024-09-24 21:50:26 +0200

    create post function for post request

---
+++ app/api/contact/route.tsx
@@ -0,0 +1,31 @@
+import { NextRequest } from "next/server";
+
+export async function POST(request: NextRequest) {
+    const apiUrl = "http://localhost:8080/contacts";
+    const formData = new FormData();
+
+    const contactData = await request.json();
+
+    formData.append("contact", JSON.stringify(contactData));
+
+    const imageFile = contactData.image;
+    if (imageFile) {
+        formData.append("image", imageFile);
+    }
+
+    try {
+        const response = await fetch(apiUrl, {
+            method: "POST",
+            body: formData,
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const savedContact = await response.json();
+        return new Response(JSON.stringify(savedContact), { status: 201 });
+    } catch (error: any) {
+        return new Response(error.message, { status: 500 });
+    }
+}
